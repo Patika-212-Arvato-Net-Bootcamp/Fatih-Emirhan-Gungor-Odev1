6d2838c69d5c0005242c4393a945f2161fdf4337
fatihemirhangungor <fatihangungor@gmail.com>
2022-06-17 23:23:38 +0300

    project-completed

---
+++ Methods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order_CLI
+{
+    class Methods
+    {
+        public bool TcControl(string tc)
+        {
+            if (tc.Length != 5)
+            {
+                Console.WriteLine("Tc must be 5 characters");
+                return false;
+            }
+            return true;
+        }
+    }
+}
